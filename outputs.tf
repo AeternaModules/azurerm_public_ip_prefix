7c3a021c04a9367471171366ae291ec2a4567663
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "public_ip_prefixes" {
-  description = "All public_ip_prefix resources"
-  value       = azurerm_public_ip_prefix.public_ip_prefixes
-}
 output "public_ip_prefixes_custom_ip_prefix_id" {
-  description = "List of custom_ip_prefix_id values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.custom_ip_prefix_id]
+  description = "Map of custom_ip_prefix_id values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.custom_ip_prefix_id }
 }
 output "public_ip_prefixes_ip_prefix" {
-  description = "List of ip_prefix values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.ip_prefix]
+  description = "Map of ip_prefix values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.ip_prefix }
 }
 output "public_ip_prefixes_ip_version" {
-  description = "List of ip_version values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.ip_version]
+  description = "Map of ip_version values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.ip_version }
 }
 output "public_ip_prefixes_location" {
-  description = "List of location values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.location]
+  description = "Map of location values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.location }
 }
 output "public_ip_prefixes_name" {
-  description = "List of name values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.name]
+  description = "Map of name values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.name }
 }
 output "public_ip_prefixes_prefix_length" {
-  description = "List of prefix_length values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.prefix_length]
+  description = "Map of prefix_length values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.prefix_length }
 }
 output "public_ip_prefixes_resource_group_name" {
-  description = "List of resource_group_name values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.resource_group_name]
+  description = "Map of resource_group_name values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.resource_group_name }
 }
 output "public_ip_prefixes_sku" {
-  description = "List of sku values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.sku]
+  description = "Map of sku values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.sku }
 }
 output "public_ip_prefixes_sku_tier" {
-  description = "List of sku_tier values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.sku_tier]
+  description = "Map of sku_tier values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.sku_tier }
 }
 output "public_ip_prefixes_tags" {
-  description = "List of tags values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.tags]
+  description = "Map of tags values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.tags }
 }
 output "public_ip_prefixes_zones" {
-  description = "List of zones values across all public_ip_prefixes"
-  value       = [for k, v in azurerm_public_ip_prefix.public_ip_prefixes : v.zones]
+  description = "Map of zones values across all public_ip_prefixes, keyed the same as var.public_ip_prefixes"
+  value       = { for k, v in azurerm_public_ip_prefix.public_ip_prefixes : k => v.zones }
 }
 
